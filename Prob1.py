d81965c2714f735d337d6057ff49a3d279f191f6
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 14:29:21 +0700

    Initial commit

---
+++ Prob1.py
@@ -0,0 +1,16 @@
+# Use Map to replace 'The' (case insensitive) with 'xxx'
+def Problem1(atext):
+   
+    ltext = atext.split(" ")
+    ####Your code here####
+    #def replace_the_map
+    ###########
+    ####Your code here, rtext must be a list####
+    #rtext = ?map?                             #
+    ############################################
+    
+    # Check your rtext does not contain 'the'
+    # if not any(map(lambda# Your code here
+        return ' '.join(rtext)
+    # else:
+        return 'still exist the word the'
---
+++ Prob2.py
@@ -0,0 +1,16 @@
+#Reduce.  Replace the word 'The', case insensitive, with 'xxx' from a text
+from functools import reduce
+def Problem2(atext):
+
+    
+    ltext = atext.split(" ")
+
+    ####Your code here####
+    #def relace_the_reduce(
+    #    return ??
+    #####################
+    ####Your code here###
+    #anstext=reduce(
+    #####################
+    return anstext
+
