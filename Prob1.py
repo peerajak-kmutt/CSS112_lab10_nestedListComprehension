ff7a4f0e2e2f3e2dc51adc0a017288c982676516
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 15:08:50 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,5 +1,42 @@
 import numpy as np
-# Use Map to replace 'The' (case insensitive) with 'xxx'
-def Problem1(N): 
+"""
+n=3
+onesk = np.ones((n,n))
+print(onesk)
+"""
 
-    return N
+#[[1. 1. 1.]
+# [1. 1. 1.]
+# [1. 1. 1.]]
+
+"""
+print('We want this matrix\n')
+print(np.array([[1.,0.,0.],[1.,1.,0.],[1.,1.,1.]]))
+"""
+#We want this matrix
+#
+#[[1. 0. 0.]
+# [1. 1. 0.]
+# [1. 1. 1.]]
+'''
+zero_xyindx = [(0,1),(0,2),(1,2)]
+xyindx = tuple(zip(*zero_xyindx))
+print(xyindx)
+print('Notice ((all x),(all y))')
+'''
+#((0, 0, 1), (1, 2, 2))
+#Notice ((all x),(all y))
+'''
+onesk[xyindx] = 0
+print(onesk)
+'''
+#[[1. 0. 0.]
+# [1. 1. 0.]
+# [1. 1. 1.]]
+
+def Problem1(n): 
+    onesk = np.ones((n,n))
+    indx = #Your code here
+    xyindx = tuple(zip(*indx))
+    onesk[xyindx] = 0
+    return onesk
