5a405da7e760b587f6937a3db674b88652986bf8
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 14:50:03 +0700

    Update Prob1_test.py

--- Prob1_test.py
+++ Prob1_test.py
@@ -2,12 +2,10 @@ import Prob1 as p1
 import Numpy as np
 
 def test_1():
-    atext='The split method is used to split the strings and store them in the list. The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. If a delimiter is not specified or is None, a different splitting algorithm is applied: runs of consecutive whitespace are regarded as a single separator, and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'
-    ans = 'xxx split method is used to split xxx strings and store them in xxx list. xxx built-in method returns a list of xxx words in xxx string, using xxx “delimiter” as xxx delimiter string. If a delimiter is not specified or is None, a different splitting algorithm is applied: runs of consecutive whitespace are regarded as a single separator, and xxx result will contain no empty strings at xxx start or end if xxx string has leading or trailing whitespace.'
-    assert p2.Problem2(atext) == ans
+    one3 = p1.Problem1(3)
+    assert (one3 == np.array([[1.,0.,0.],[1.,1.,0.],[1.,1.,1.]])).all()
 
-def test_1():
-    atext='The split method is used to split the strings and store them in the list. The built-in method returns a list of the words in the string, using the “delimiter” as the delimiter string. If a delimiter is not specified or is None, a different splitting algorithm is applied: runs of consecutive whitespace are regarded as a single separator, and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.'
-    ans = 'xxx split method is used to split xxx strings and store them in xxx list. xxx built-in method returns a list of xxx words in xxx string, using xxx “delimiter” as xxx delimiter string. If a delimiter is not specified or is None, a different splitting algorithm is applied: runs of consecutive whitespace are regarded as a single separator, and xxx result will contain no empty strings at xxx start or end if xxx string has leading or trailing whitespace.'
-    assert p1.Problem1(atext) == ans
+def test_2():
+    one4 = p1.Problem1(4)
+    assert (one4 == np.array([[1.,0.,0.,0.],[1.,1.,0.,0.],[1.,1.,1.,0.],[1.,1.,1.,1.]])).all()
 
