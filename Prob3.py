e1a32ae0ca4be24b5294c0f9cfd24e01b6ad4826
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 14:52:09 +0700

    Update Prob3.py

--- Prob3.py
+++ Prob3.py
@@ -2,4 +2,4 @@ def Problem3(N):
   primes = list_of_primes(N)
   dict_of_primes = dict(zip(range(len(primes)),primes))
   odd_dict_of_primes =  #Your Code Here  
-  return N
+  return odd_dict_of_primes
