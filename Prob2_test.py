acd54789a16923ce24d75a17b49ae7f49abd7088
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 14:39:19 +0700

    Update Prob2_test.py

--- Prob2_test.py
+++ Prob2_test.py
@@ -1,11 +1,11 @@
 import Prob2 as p2;
 
 def test_1():
-    primes = list_of_primes(100)
+    primes = p2.Problem2(100)
     assert primes == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     
 def test_2()
-    primes = list_of_primes(50)
+    primes = p2.Problem2(50)
     assert primes == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
 
