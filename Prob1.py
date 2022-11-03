2ba049662f1c31eb4a20bcc2670aba9ba2265761
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-11-03 15:35:00 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,38 +1,5 @@
 import numpy as np
-"""
-n=3
-onesk = np.ones((n,n))
-print(onesk)
-"""
 
-#[[1. 1. 1.]
-# [1. 1. 1.]
-# [1. 1. 1.]]
-
-"""
-print('We want this matrix\n')
-print(np.array([[1.,0.,0.],[1.,1.,0.],[1.,1.,1.]]))
-"""
-#We want this matrix
-#
-#[[1. 0. 0.]
-# [1. 1. 0.]
-# [1. 1. 1.]]
-'''
-zero_xyindx = [(0,1),(0,2),(1,2)]
-xyindx = tuple(zip(*zero_xyindx))
-print(xyindx)
-print('Notice ((all x),(all y))')
-'''
-#((0, 0, 1), (1, 2, 2))
-#Notice ((all x),(all y))
-'''
-onesk[xyindx] = 0
-print(onesk)
-'''
-#[[1. 0. 0.]
-# [1. 1. 0.]
-# [1. 1. 1.]]
 
 def Problem1(n): 
     onesk = np.ones((n,n))
